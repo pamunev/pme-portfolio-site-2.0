52191c5017ebb487e8da614d02095392b3da7851
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-02-16 11:36:41 -0600

    Changed some fonts.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -8,10 +8,10 @@ function AboutSection() {
         <div className="content-flex-container">
           <p className="bio-paragraph">
             I’m a <b>full-stack software developer</b> living in the beautiful,
-            hot city of Austin, Texas. Aside from my proficiency in full-stack
-            web development (<b>React, Node.js</b>, etc.), I’ve also developed
-            strong skills in collaboration, communication, and quality,
-            customer-centered product creation through a successful and
+            blazing hot city of Austin, Texas. Aside from my proficiency in
+            full-stack web development (<b>React, Node.js</b>, etc.), I’ve also
+            developed strong skills in collaboration, communication, and
+            quality, customer-centered product creation through a successful and
             dedicated career in education and social media marketing. I am also
             active in the Austin theatre scene as an actor and a playwright.
             <br />
