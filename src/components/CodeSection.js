49276c62e4a6a17ba9ed760dd20f7905747bcd0e
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2024-02-18 21:51:46 -0600

    Added footer elements and styled them.

--- src/components/CodeSection.js
+++ src/components/CodeSection.js
@@ -17,7 +17,30 @@ function CodeSection() {
             Periodic Tables
           </a>{" "}
           is a full-stack reservation and table management system for restaurant
-          employees to use.
+          employees to use. Built with React and Node + Express.
+        </p>
+        <p>
+          <a
+            href="https://github.com/pamunev/we-love-movies-backend-capstone"
+            target="_blank"
+            rel="noreferrer"
+          >
+            WeLoveMovies
+          </a>{" "}
+          is the back-end of a movie website that allows users to search for and
+          review their favorite movies. Built with Node + Express.
+        </p>
+        <p>
+          My{" "}
+          <a
+            href="https://github.com/pamunev/flashcard-app-pablo-munoz-evers"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Flashcard App
+          </a>{" "}
+          is a study tool that allows users to create and manage study card
+          decks. Built with React and Node + Express.
         </p>
       </div>
     </div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,39 @@
 import React from "react";
+import github from "../assets/images/github-logo.svg";
+import linkedin from "../assets/images/linkedin-logo-svgrepo-com.svg";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <div>Footer</div>
+      <div>
+        Copyright 2024 © Pablo Munoz-Evers. Created with React and CSS -{" "}
+        <a
+          href="https://github.com/pamunev/pme-portfolio-site-2.0"
+          target="_blank"
+          rel="noreferrer"
+        >
+          view source code here.
+        </a>
+      </div>
+      <hr />
+      <div>
+        <a
+          title="LinkedIn"
+          href="https://github.com/pamunev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={github} alt="GitHub Icon" />
+        </a>
+        <a
+          title="LinkedIn"
+          href="https://www.linkedin.com/in/pablo-munoz-evers/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedin} alt="LinkedIn Icon" />
+        </a>
+      </div>
     </div>
   );
 }
